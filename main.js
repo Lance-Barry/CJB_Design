bc3ffcb73c6442b7b7d6259c11693d14deec038e
Lance B <bhmtnbiker@hotmail.com>
2019-07-07 18:20:12 -0500

    Added Art page and worked on responsive design

---
+++ main.js
@@ -0,0 +1,47 @@
+const parallax = document.getElementById("parallax");
+
+// window.addEventListener("scroll", function() {
+//   let offset = window.pageYOffset;
+//   parallax.style.backgroundPositionY = offset * .7 + " px";
+// });
+
+// var phoneModal = document.getElementById("myPhoneModal");
+// var emailModal = document.getElementById("myEmailModal");
+// var emailBtn = document.getElementById("email");
+// var phoneBtn = document.getElementById("phone");
+
+// var span = document.getElementsByClassName("closeEmail")[0];
+// var span2 = document.getElementsByClassName("closePhone")[0]
+
+// phoneBtn.onclick = function() {
+//   phoneModal.style.display = "block";
+// };
+
+// emailBtn.onclick = function() {
+//   emailModal.style.display = "block";
+// };
+
+// span.onclick = function() {
+
+//     emailModal.style.display = "none";
+// };
+
+// span2.onclick = function(){
+//     phoneModal.style.display = "none"
+// }
+
+// //Close modal when click anywhere outside of modal
+// window.onclick = function(event) {
+//   if (event.target == phoneModal) {
+//     phoneModal.style.display = "none";
+//   } else if (event.target == emailModal) {
+//     emailModal.style.display = "none";
+//   }
+// };
+
+
+
+
+
+
+
